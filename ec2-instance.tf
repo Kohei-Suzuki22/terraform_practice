b68a897137710e59971e88e17c6290831c4c7f40
kohei <bhkbb0551@gmail.com>
2023-05-04 13:47:24 +0900

    terraformの基本コマンドとその概要

--- ec2-instance.tf
+++ ec2-instance.tf
@@ -14,8 +14,32 @@ provider "aws" {
   region = "ap-northeast-1"
 }
 
-# Resource lock
+# Resource Block
 resource "aws_instance" "ec2demo" {
   ami = "ami-0e0820ad173f20fbb" #amazon Linux in ap-northeast-1, update as
   instance_type = "t2.micro"
 }
+
+
+
+# terraformコマンドの基本コマンド
+
+## terraform init
+### -> 作業ディレクトリのリソース作成に必要なプラグインファイルをダウンロードしてくる。
+
+## terraform validate
+### -> 作業ディレクトリのtfファイルの宣言が、有効であるか検証する。(文法的なミスを確認できる)
+
+## terraform plan
+### -> 作業ディレクトリのtfファイルの適用でリソースがどのように変化するのかを出力する。(現状のtfstateとの状態の違いを表示する)
+### -> terraform apply の前に実行する必要がある。
+
+## terraform apply
+### -> 作業ディレクトリのtfファイルを適用する。
+### -> リソースを実際に追加・変更・削除する。
+
+## terraform destroy
+### -> 作業ディレクトリのtfファイルの内容をリソースから削除する。(tfstateの状態からtfファイルの内容を削除)
+
+
+
